d1ee7ddc4360e157a8fc806ccc1f1ee26a6681b5
gulsah-hezer <66849531+gulsah-hezer@users.noreply.github.com>
2020-07-13 21:57:38 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,15 @@
 
+#STEP 1: Write a function that can print out a board. Set up your board as a list where each index 1-9 with a number on a number pad.
+#So you get a 3 by 3 board representation.
+
+#STEP 2: Write a function that can take in a player input and assign their marker  as 'X' or 'O'.Think about using while loops to continually ask
+#until you get a correct answer.
+
+#STEP 3: Write a function that takes in the board list object  a marker ('X' or 'O') and desired position (number 1-9) and assign it to board.
+
+#STEP 4: Write a function that takes in a board and a mark ('X or 'O') and then checks to see if that mark has won.
+
+#STEP 5: Write a function that uses the random module to randomly decide which player goes first.You may want to lookup random.randint().Return a string of 
+#which player went first.
+
+#STEP 6: Write a function that returns a boolean indicating whether a space on the board is freely available.
